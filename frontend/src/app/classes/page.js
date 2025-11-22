d98c99c98c1a60c27e17c183732e43a4403764c4
Kynn274 <kimpham22072004@gmail.com>
2025-11-22 16:09:08 +0700

    .env

--- frontend/src/app/classes/page.js
+++ frontend/src/app/classes/page.js
@@ -1,4 +1,5 @@
 import Header from "@/components/header";
+import ClassCard from "@/components/class_card";
 
 export default function Classes(){
     return(
@@ -7,6 +8,9 @@ export default function Classes(){
             <div className="pt-[45px]">
                 <div className="tools px-8 py-4 w-full flex flex-row justify-end">
                     <button className="w-[150px] p-2 text-white font-medium text-sm bg-indigo-500 rounded-lg">Tạo lớp học</button>
+                </div>
+                <div>
+                    <ClassCard classes={{name: "Lớp học 1"}}/>
                 </div>
             </div>
         </>
--- frontend/src/components/class_card.js
+++ frontend/src/components/class_card.js
@@ -1,7 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis, faSquarePhone, faBook } from "@fortawesome/free-solid-svg-icons";
+
 export default function CurrentClassesCard({ classes }) {
     return (
-        <div className="course-item border border-gray-300 rounded-lg p-3 shadow-sm hover:shadow-md transition-shadow duration-300 w-[13rem] mx-auto relative">
-            <h3 className="font-semibold text-lg text-gray-800">{classes.name}</h3>
+        <div className="course-item border border-emerald-200 bg-emerald-700 rounded-lg p-3 shadow-xs hover:shadow-md transition-shadow duration-200 w-[250px] h-[120px] mx-auto relative">
+            <h3 className="absolute bottom-0 left-0 font-semibold text-lg text-white py-3 px-4">{classes.name}</h3>
+            <div className="relative group/icon">
+                <FontAwesomeIcon icon={faEllipsis} className="text-white !w-[20px] absolute top-0 right-0 transition hover:cursor-pointer" />
+                <div className="bg-white border border-gray-400 shadow-2xs absolute top-0 -right-10">
+                    <li>
+                        <span>Ẩn lớp học</span>
+                    </li>
+                    <li>
+                        
+                    </li>
+                </div>
+            </div>
+            <div className="text-white flex flex-row gap-2 absolute top-0 left-0">
+                <div className="relative group/icon">
+                    <FontAwesomeIcon icon={faSquarePhone} className="text-white !w-[20px] py-3 pl-3 transition hover:cursor-pointer" />
+                    <span className="absolute -top-6 left-1/2 -translate-x-1/2 text-xs text-nowrap text-gray-600 hidden group-hover/icon:block">Gọi điện</span>
+                </div>
+                <div className="relative group/icon">
+                    <FontAwesomeIcon icon={faBook} className="text-white !w-[20px] py-3 pr-3 transition hover:cursor-pointer" />
+                    <span className="absolute -top-6 left-1/2 -translate-x-1/2 text-xs text-nowrap text-gray-600 hidden group-hover/icon:block">Bài tập</span>
+                </div>
+            </div>
         </div>
     );
-}
+} 
